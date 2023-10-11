d4408e391bf8f25921393760ef4ef25a06e46b5c
Burak İpekci <burakipekci@outlook.com>
2023-10-11 16:38:06 +0300

    Brand Command added

---
+++ src/rentACar/RentACar/WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,13 @@
+﻿using Application.Features.Brands.Command.Create;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BrandsController : ControllerBase
+{
+	[HttpPost]
+	public async Task<IActionResult> Add (CreateBrandCommand)
+}
