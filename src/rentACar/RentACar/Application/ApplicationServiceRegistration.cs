5cd0b8148ab3367bf9a9a1dc2f3af8eaf273b782
Burak İpekci <burakipekci@outlook.com>
2023-10-12 12:41:08 +0300

    AsyncRepo ,IQuery added

--- src/rentACar/RentACar/Application/ApplicationServiceRegistration.cs
+++ src/rentACar/RentACar/Application/ApplicationServiceRegistration.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,4 +11,14 @@ namespace Application;
 
 public class ApplicationServiceRegistration
 {
+	public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+	{
+		services.AddMediatR(configuration =>
+		{
+			configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+
+
+		});
+		return services;
+	}
 }
